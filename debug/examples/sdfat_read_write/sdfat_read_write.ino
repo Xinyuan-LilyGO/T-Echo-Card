acde762450a7cd903dc6e7886ee86d8831f2f2d9
LILYGO_L <13073925292@163.com>
2026-01-07 10:01:39 +0800

    add sdfat_read_write example

---
+++ debug/examples/sdfat_read_write/sdfat_read_write.ino
@@ -0,0 +1,129 @@
+/*
+ * @Description: None
+ * @Author: LILYGO_L
+ * @Date: 2026-01-07 09:50:04
+ * @LastEditTime: 2026-01-07 09:54:21
+ * @License: GPL 3.0
+ */
+
+#include <SPI.h>
+#include "SdFat.h"
+#include "Adafruit_SPIFlash.h"
+#include "t_echo_card_config.h"
+
+// QSPI
+Adafruit_FlashTransport_QSPI flashTransport(ZD25WQ32C_SCLK, ZD25WQ32C_CS,
+                                            ZD25WQ32C_IO0, ZD25WQ32C_IO1,
+                                            ZD25WQ32C_IO2, ZD25WQ32C_IO3);
+
+Adafruit_SPIFlash flash(&flashTransport);
+
+SPIFlash_Device_t ZD25WQ32C =
+    {
+        total_size : (1UL << 22), /* 4 MiB */
+        start_up_time_us : 12000,
+        manufacturer_id : 0xBA,
+        memory_type : 0x60,
+        capacity : 0x16,
+        max_clock_speed_mhz : 104,
+        quad_enable_bit_mask : 0x02,
+        has_sector_protection : false,
+        supports_fast_read : true,
+        supports_qspi : true,
+        supports_qspi_writes : true,
+        write_status_register_split : false,
+        single_status_byte : false,
+        is_fram : false,
+    };
+
+// file system object from SdFat
+FatVolume fatfs;
+File32 myFile;
+
+void setup()
+{
+    // Open serial communications and wait for port to open:
+    Serial.begin(115200);
+    while (!Serial)
+    {
+        delay(10); // wait for serial port to connect. Needed for native USB port only
+    }
+    Serial.println("Ciallo");
+
+    // 3.3V Power ON
+    pinMode(RT9080_EN, OUTPUT);
+    digitalWrite(RT9080_EN, HIGH);
+
+    delay(500);
+
+    Serial.println("Initializing Filesystem on external flash...");
+
+    // Init external flash
+    while (flash.begin(&ZD25WQ32C) == false)
+    {
+        Serial.println("Flash initialization failed");
+        delay(1000);
+    }
+    Serial.println("Flash initialization successful");
+
+    // QSPI
+    flashTransport.setClockSpeed(32000000UL, 0);
+
+    // Open file system on the flash
+    if (!fatfs.begin(&flash))
+    {
+        Serial.println("Error: filesystem is not existed. Please try SdFat_format "
+                       "example to make one.");
+        while (1)
+        {
+            yield();
+            delay(1);
+        }
+    }
+
+    Serial.println("initialization done.");
+
+    // open the file. note that only one file can be open at a time,
+    // so you have to close this one before opening another.
+    myFile = fatfs.open("test.txt", FILE_WRITE);
+
+    // if the file opened okay, write to it:
+    if (myFile)
+    {
+        Serial.print("Writing to test.txt...");
+        myFile.println("testing 1, 2, 3.");
+        // close the file:
+        myFile.close();
+        Serial.println("done.");
+    }
+    else
+    {
+        // if the file didn't open, print an error:
+        Serial.println("error opening test.txt");
+    }
+
+    // re-open the file for reading:
+    myFile = fatfs.open("test.txt");
+    if (myFile)
+    {
+        Serial.println("test.txt:");
+
+        // read from the file until there's nothing else in it:
+        while (myFile.available())
+        {
+            Serial.write(myFile.read());
+        }
+        // close the file:
+        myFile.close();
+    }
+    else
+    {
+        // if the file didn't open, print an error:
+        Serial.println("error opening test.txt");
+    }
+}
+
+void loop()
+{
+    // nothing happens after setup
+}
